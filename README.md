561f4adb32d6f5cb458d54bc955a109320d1ef9b
Mernus <mernusv@gmail.com>
2020-10-29 19:07:06 +0400

    Iterator pattern realization and README update

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-Design patterns realized on Python
+# Summary
+This project contains design patterns realized on Python.
+
+## Download the Project
+
+First, clone the repository to your local machine:
+
+```bash
+git clone git@github.com:Mernus/design_patterns.git
+```
+
+## Executing examples
+
+In project directory you can find compiled versions of design patterns files
+(files *.pyc)
+
+To execute this files and see the output of design patterns examples you need to print in console
+
+```bash
+python file.pyc
+```
+OR
+```bash 
+py file.pyc
+```
+
+## Sources
+
+Also you can see uncompiled files in directory with corresponding name('uncompiled')
+Names of uncompiled files is the same to compiled.
---
+++ uncompiled/iterator.py
@@ -0,0 +1,78 @@
+from collections.abc import Iterable, Iterator
+from typing import Any, List
+
+
+class AlphabeticalIterator(Iterator):
+    """
+    Class that realize iterator in alphabetical order for any iterable collection
+
+    """
+
+    def __init__(self, iterable_object: object, reverse: bool = False) -> None:
+        """
+        Initialize iterator for iterable collection
+
+        :param iterable_object: object to iterate
+        :param reverse: reverse or not iteration order
+        """
+        self._current_index = -1 if reverse else 0
+        self._collection = iterable_object
+        self._reverse = reverse
+
+    def __next__(self) -> object:
+        """
+        Returns next object in alphabetical order
+
+        :return: next object of collection
+        """
+        try:
+            value = self._collection[self._current_index]
+            self._current_index += -1 if self._reverse else 1
+
+        except IndexError:
+            raise StopIteration()
+
+        return value
+
+
+class IterableCollection(Iterable):
+    """
+    Iterable collection with list type to implement Iterator pattern
+
+    """
+
+    def __init__(self, collection: List[Any] = None) -> None:
+        self._collection = [] if collection is None else collection
+
+    def __iter__(self) -> AlphabeticalIterator:
+        """
+        Returns iterator object
+
+        :return: iterator
+        """
+        return AlphabeticalIterator(self._collection)
+
+    def __reversed__(self) -> AlphabeticalIterator:
+        """
+        Returns reversed iterator object
+
+        :return: reversed iterator
+        """
+        return AlphabeticalIterator(self._collection, True)
+
+    def push(self, item: Any):
+        self._collection.append(item)
+
+
+if __name__ == "__main__":
+    collection = IterableCollection()
+    collection.push("Red")
+    collection.push("Orange")
+    collection.push("Green")
+
+    print("\nDefault order: {")
+    print("\n".join(collection), end="\n}\n")
+
+    reversed_collection = reversed(collection)
+    print("Reversed order: {")
+    print("\n".join(reversed_collection), end="\n}\n")
